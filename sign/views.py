6a87ca11e825c3a9b5e85b2e055a3dfdeda899d7
jiayu520 <jiayu520000@outtlook.com>
2020-06-28 19:30:40 +0800

    first commit

--- sign/views.py
+++ sign/views.py
@@ -1,8 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
 
 # Create your views here.
 # def index(request):
 #     return HttpResponse("Hello Django! ")
 def index(request):
     return render(request,"index.html")
+
+#登录操作
+def login_action(request):
+    if request.method == 'POST':
+        username = request.POST.get('username','')
+        password = request.POST.get('password','')
+        if username == 'admin' and password == 'admin123':
+            #return HttpResponseRedirect('/event_manage')
+            response = HttpResponseRedirect('/event_manage/')
+            #response.set_cookie('user',username,3600)#添加浏览器cookie
+            request.seesion['user'] = username#将seesion信息记录到浏览器
+            return response
+        else:
+            return render(request,'index.html', {'error': 'username or password error!'})
+
+#发布会管理
+def event_manage(request):
+    #username = request.COOKIES.get('user','')#读取浏览器cookie
+    username = request.seesion.get('user','')#读取浏览器session
+    return render(request,"event_manage.html",{"user":username})
