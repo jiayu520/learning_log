12a0688e29959481a4533337e2c5350b7d34e6bc
jiayu520 <jiayu520000@outtlook.com>
2020-06-29 18:14:02 +0800

    database

--- sign/admin.py
+++ sign/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from sign.models import Event, Guest
+
 
 # Register your models here.
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'status', 'address', 'start_time']
+    search_fields = ['name']#搜索栏
+
+
+
+class GuestAdmin(admin.ModelAdmin):
+    list_display = ['realname', 'phone', 'email', 'sign', 'create_time', 'event']
+
+
+admin.site.register(Event)
+admin.site.register(Guest)
--- sign/views.py
+++ sign/views.py
@@ -1,5 +1,7 @@
+from django.contrib import auth
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -13,17 +15,25 @@ def login_action(request):
     if request.method == 'POST':
         username = request.POST.get('username','')
         password = request.POST.get('password','')
-        if username == 'admin' and password == 'admin123':
-            #return HttpResponseRedirect('/event_manage')
+        # if username == 'admin' and password == 'admin123':
+        #     #return HttpResponseRedirect('/event_manage')
+        #     response = HttpResponseRedirect('/event_manage/')
+        #     #response.set_cookie('user',username,3600)#添加浏览器cookie
+        #     request.session['user'] = username#将seesion信息记录到浏览器
+        #     return response
+        # else:
+        #     return render(request,'index.html', {'error': 'username or password error!'})
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request,user)#登录
+            request.session['user'] = username#将session信息记录到浏览器
             response = HttpResponseRedirect('/event_manage/')
-            #response.set_cookie('user',username,3600)#添加浏览器cookie
-            request.seesion['user'] = username#将seesion信息记录到浏览器
             return response
         else:
-            return render(request,'index.html', {'error': 'username or password error!'})
-
+            return render(request,'index.html',{'error':'username or password error!'})
 #发布会管理
+@login_required()
 def event_manage(request):
     #username = request.COOKIES.get('user','')#读取浏览器cookie
-    username = request.seesion.get('user','')#读取浏览器session
+    username = request.session.get('user','')#读取浏览器session
     return render(request,"event_manage.html",{"user":username})
